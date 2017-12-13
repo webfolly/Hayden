2610a8b95f69311646285ca7ef096ba9c72a785e
webfolly <zhanghuadong2015@gmail.com>
2017-12-13 14:51:01 +0800

    introduce responsive design to adapt various screen resolutions

--- js/app.js
+++ js/app.js
@@ -1,54 +1,31 @@
-/*console.log('Hello World');
-var myName='张华东';
-console.log(myName);
-alert(myName);*/
 
-function aboutMeToggle(){
-    var aboutMe =  document.getElementById("aboutMe");
-    if(aboutMe.style.display=="none"){
-        aboutMe.style.display="block";
-    }
+function showMobileMenu(){  
+    var mobileMenuIcon = document.getElementById("mobile-menu-icon");
+    var mobileMenuContent = document.getElementById("mobile-menu-content");  
+    if(mobileMenuContent.className === "mobile-menu-content"){
+        mobileMenuContent.className = "mobile-menu-content mobile-visible";
+        mobileMenuIcon.className = "fa fa-times"
+    }        
     else{
-        aboutMe.style.display="none";
+        mobileMenuContent.className = "mobile-menu-content";
+        mobileMenuIcon.className = "fa fa-bars"
     }
-
 }
 
-function changeName(){
-    if(document.getElementById("myName").innerHTML=="Hayden Zhang"){
-        document.getElementById("myName").innerHTML = "张华东";
+/*
+window.onclick = function(event) {
+    var moileMenu = document.getElementById("mobile-menu")
+    var mobileMenuContent = document.getElementById("mobile-menu-content");
+    if (event.target === moileMenu) {
+        if(mobileMenuContent.className === "mobile-menu-content"){
+            mobileMenuContent.className = "mobile-menu-content mobile-visible";
+        }
+        else{
+            mobileMenuContent.className = "mobile-menu-content";
+        } 
     }
-    else{
-        document.getElementById("myName").innerHTML = "Hayden Zhang";
-    }
-   
+    else {
+        mobileMenuContent.className = "mobile-menu-content";
+    }   
 }
-
-function mOver(){
-    var image =  document.getElementById("myGithub");
-    image.className="set-link-style";
-    image.innerHTML = "https://github.com/webfolly/Hayden";
-}
-function mOut(){
-    document.getElementById("myGithub").innerHTML = "";
-}
-
-function showModal(){
-    document.getElementById("myModal").style.display = "block";
-}
-function closeModal(){
-    document.getElementById("myModal").style.display = "none"; 
-}
-
-function modalSubmit(){
-    var name = document.getElementById("name");
-    var email = document.getElementById("email");
-    var message = document.getElementById("message");
-    console.log("Name: "+name.value);
-    console.log("email: "+email.value);
-    console.log("message: "+message.value);
-    name.value = name.getAttribute("placeholder");
-    email.value = email.getAttribute("placeholder");
-    message.value = message.getAttribute("placeholder");
-    closeModal();
-}
+*/
